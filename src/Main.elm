fa0f221e24359177630f2dcd76a858520c0d51e9
Stefan Rimaila <stefan@fusetools.com>
2016-09-04 23:10:49 +0300

    Relocate main app to src/App

--- src/Main.elm
+++ src/Main.elm
@@ -7,15 +7,15 @@ Implementation in the Elm Architecture.
 Check out <http://guide.elm-lang.org/architecture/index.html> for more info.
 -}
 
-import Html.App as App
-import Types exposing (Model)
-import State exposing (init, subscriptions, update, withSetStorage)
-import View exposing (view)
+import Html.App as Application
+import App.Types exposing (Model)
+import App.State exposing (init, subscriptions, update, withSetStorage)
+import App.View exposing (view)
 
 
 main : Program (Maybe Model)
 main =
-    App.programWithFlags
+    Application.programWithFlags
         { init = init
         , view = view
         , update = (\msg model -> withSetStorage <| update msg model)
